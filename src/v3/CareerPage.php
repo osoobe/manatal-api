d548f31db018f0bf3899be320de55ffd174cc1cb
Oshane Bailey <b4.oshany@gmail.com>
2021-11-08 07:10:21 +0000

    fix: added try catch for getJobs

--- src/v3/CareerPage.php
+++ src/v3/CareerPage.php
@@ -5,12 +5,17 @@ namespace Osoobe\Manatal\v3;
 class CareerPage extends API {
 
     public static function getJobs($client_slug) {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', static::API_URL."/career-page/$client_slug/jobs/");
-        $data  = $response->getBody()->getContents();
-        if ( is_string($data) ) {
-            return json_decode($data, true);
+        try {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request('GET', static::API_URL."/career-page/$client_slug/jobs/");
+            $data  = $response->getBody()->getContents();
+            if ( is_string($data) ) {
+                return json_decode($data, true);
+            }
+        } catch (\Throwable $th) {
+            $data = [ 'detail' => 'Not Found', 'error' => $th->getMessage()];
         }
+        $data = [];
         return $data;
     }
 
